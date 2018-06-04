94ae6c243d1bfa691429e366e624818f78977faa
Bartek Malanowski <b.malanowski@mdbootstrap.com>
2018-06-04 11:07:21 +0200

    4.5.4 release

--- js/mdb.js
+++ js/mdb.js
@@ -1,6 +1,6 @@
 /*!
  * Material Design for Bootstrap 4
- * Version: MDB FREE: 4.5.3
+ * Version: MDB FREE: 4.5.4
  *
  *
  * Copyright: Material Design for Bootstrap
@@ -15181,6 +15181,7 @@ $(window).scroll(function () {
 //Initialization
 Waves.attach('.btn:not(.btn-flat), .btn-floating', ['waves-light']);
 Waves.attach('.btn-flat', ['waves-effect']);
+Waves.attach('.chip', ['waves-effect']);
 Waves.attach('.view a .mask', ['waves-light']);
 Waves.attach('.waves-light', ['waves-light']);
 Waves.attach('.navbar-nav a:not(.navbar-brand), .nav-icons li a, .nav-tabs .nav-item:not(.dropdown)', ['waves-light']);
